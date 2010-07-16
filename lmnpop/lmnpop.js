7e2237db2d4ff7bd5f3a94bd0b2a57fc15ac9183
satyr <murky.satyr@gmail.com>
2010-07-16 15:06:30 +0900

    compacted the data url

--- lmnpop/lmnpop.js
+++ lmnpop/lmnpop.js
@@ -3,25 +3,26 @@ var [lmn] = this.arguments;
 if(!(lmn instanceof Element)){
   var [url, slc] = typeof lmn == 'string' ? [lmn] : lmn;
   slc = slc || lmnpop.pget('selector');
+  document.title = lmnpop.trim(url);
 }
 var brw = document.getElementById('browser');
 brw.setAttribute('src', url || (
-  'data:text/html,<!--lmnpop--><style>html,body{height:100%;margin:0}</style>'+
+  'data:text/html,<!--lmnpop-->'+
   encodeURI(<base href={lmn.baseURI}/>.toXMLString())));
 brw.addEventListener('DOMContentLoaded', function lmnset(ev){
-  var doc = content.document;
+  var doc = content.document, htm = doc.documentElement;
   if(slc){
-    lmn = doc.querySelector(slc);
-    if(!lmn) return;
-    let htm = doc.documentElement;
+    if(!(lmn = doc.querySelector(slc))) return;
     while(htm.hasChildNodes()) htm.removeChild(htm.lastChild);
-    htm.setAttribute('style', 'height:100%');
     htm.appendChild(lmn);
   } else {
-    lmn = doc.body.appendChild(doc.adoptNode(lmn));
+    let {body} = doc;
+    body.setAttribute('style', 'margin:0');
+    lmn = body.appendChild(doc.adoptNode(lmn));
     slc = lmn.id ? '#'+ lmn.id : (
       lmn.tagName + [''].concat(Array.slice(lmn.classList)).join('.'));
   }
+  htm.setAttribute('style', 'height:100%');
   lmn.setAttribute(
     'style', 'width:100%;height:100%;display:block;overflow:auto');
   document.title = lmnpop.format(lmn);
