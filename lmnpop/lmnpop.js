1edd144374bcd727c4850d1f5d25cd238b508df7
satyr <murky.satyr@gmail.com>
2010-07-23 07:56:32 +0900

    now loads within onload(), fixing a rendering issue on Fx4.0+

--- lmnpop/lmnpop.js
+++ lmnpop/lmnpop.js
@@ -5,25 +5,27 @@ if(!(lmn instanceof Element)){
   slc = slc || lmnpop.pget('selector');
   document.title = lmnpop.trim(url);
 }
-var brw = document.getElementById('browser');
-brw.setAttribute('src', url || (
-  'data:text/html,<!--lmnpop-->'+
-  encodeURI(<base href={lmn.baseURI}/>.toXMLString())));
-brw.addEventListener('DOMContentLoaded', function lmnset(ev){
-  var doc = content.document, htm = doc.documentElement;
-  if(slc){
-    if(!(lmn = doc.querySelector(slc))) return;
-    while(htm.hasChildNodes()) htm.removeChild(htm.lastChild);
-    htm.appendChild(lmn);
-  } else {
-    let {body} = doc;
-    body.setAttribute('style', 'margin:0');
-    lmn = body.appendChild(doc.adoptNode(lmn));
-    slc = lmn.id ? '#'+ lmn.id : (
-      lmn.tagName + [''].concat(Array.slice(lmn.classList)).join('.'));
-  }
-  htm.setAttribute('style', 'height:100%');
-  lmn.setAttribute(
-    'style', 'width:100%;height:100%;display:block;overflow:auto');
-  document.title = lmnpop.format(lmn);
-}, true);
+function onload(){
+  var brw = document.getElementById('browser');
+  brw.setAttribute('src', url || (
+    'data:text/html,<!--lmnpop-->'+
+    encodeURI(<base href={lmn.baseURI}/>.toXMLString())));
+  brw.addEventListener('DOMContentLoaded', function lmnset(ev){
+    var doc = content.document, htm = doc.documentElement;
+    if(slc){
+      if(!(lmn = doc.querySelector(slc))) return;
+      while(htm.hasChildNodes()) htm.removeChild(htm.lastChild);
+      htm.appendChild(lmn);
+    } else {
+      let {body} = doc;
+      body.setAttribute('style', 'margin:0');
+      lmn = body.appendChild(doc.adoptNode(lmn));
+      slc = lmn.id ? '#'+ lmn.id : (
+        lmn.tagName + [''].concat(Array.slice(lmn.classList)).join('.'));
+    }
+    htm.setAttribute('style', 'height:100%');
+    lmn.setAttribute(
+      'style', 'width:100%;height:100%;display:block;overflow:auto');
+    document.title = lmnpop.format(lmn);
+  }, true);
+}
