28c1119b10b462eec26cfedd3daa0134c8c984d6
satyr <murky.satyr@gmail.com>
2011-12-05 16:04:14 +0900

    restyled lmnpop.js

--- lmnpop/lmnpop.js
+++ lmnpop/lmnpop.js
@@ -1,31 +1,34 @@
-var {lmnpop} = opener;
-var [lmn] = this.arguments;
+var {lmnpop} = opener
+  , [lmn] = this.arguments
 if(!(lmn instanceof Element)){
-  var [url, slc] = typeof lmn == 'string' ? [lmn] : lmn;
-  slc = slc || lmnpop.pget('selector');
-  document.title = lmnpop.trim(url);
+  var [url, slc] = typeof lmn == 'string' ? [lmn] : lmn
+  slc = slc || lmnpop.pget('selector')
+  document.title = lmnpop.trim(url)
 }
-function onload(){
-  var brw = document.getElementById('browser');
-  brw.setAttribute('src', url || (
+this.onload = function onload(){
+  var brw = document.getElementById('browser')
+  brw.setAttribute('src', url ||
     'data:text/html,<!--lmnpop-->'+
-    encodeURI(<base href={lmn.baseURI}/>.toXMLString())));
+    encodeURIComponent(<base href={lmn.baseURI}/>.toXMLString()))
   brw.addEventListener('DOMContentLoaded', function lmnset(ev){
-    var doc = content.document, htm = doc.documentElement;
+    var doc = content.document
+      , htm = doc.documentElement
     if(slc){
-      if(!(lmn = doc.querySelector(slc))) return;
-      while(htm.hasChildNodes()) htm.removeChild(htm.lastChild);
-      htm.appendChild(lmn);
+      if(!(lmn = doc.querySelector(slc))) return
+      while(htm.hasChildNodes()) htm.removeChild(htm.lastChild)
+      htm.appendChild(lmn)
     } else {
-      let {body} = doc;
-      body.setAttribute('style', 'margin:0');
-      lmn = body.appendChild(doc.adoptNode(lmn));
-      slc = lmn.id ? '#'+ lmn.id : (
-        lmn.tagName + [''].concat(Array.slice(lmn.classList)).join('.'));
+      let {body} = doc
+      body.setAttribute('style', 'margin:0')
+      lmn = body.appendChild(doc.adoptNode(lmn))
+      slc = lmn.id
+          ? '#'+ lmn.id
+          : lmn.tagName + [''].concat(Array.slice(lmn.classList)).join('.')
     }
-    htm.setAttribute('style', 'height:100%');
+    htm.setAttribute(
+      'style', 'height:100%')
     lmn.setAttribute(
-      'style', 'width:100%;height:100%;display:block;overflow:auto');
-    document.title = lmnpop.format(lmn);
-  }, true);
+      'style', 'width:100%;height:100%;display:block;overflow:auto')
+    document.title = lmnpop.format(lmn)
+  }, true)
 }
