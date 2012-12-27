863dcc3cd158c11cf7570ad831a4ca215049b8b7
satyr <murky.satyr@gmail.com>
2012-12-27 21:21:18 +0900

    made #5 work for <object>s

--- lmnpop/lmnpop.js
+++ lmnpop/lmnpop.js
@@ -23,10 +23,9 @@ this.onload = function onload(){
           ? '#'+ lmn.id
           : lmn.tagName + [''].concat(Array.slice(lmn.classList)).join('.')
     }
-    Array.slice(doc.plugins).forEach(function(plugin){
-      plugin.QueryInterface(Components.interfaces.nsIObjectLoadingContent)
-      if (!plugin.activated) plugin.playPlugin()
-    })
+    for(let plugin of doc.querySelectorAll('embed, object'))
+      if(plugin instanceof Components.interfaces.nsIObjectLoadingContent)
+        plugin.activated || plugin.playPlugin()
     function set(target){
       document.title = lmnpop.format(lmn)
       if(lmn.parentNode) lmn.parentNode.removeChild(lmn)
